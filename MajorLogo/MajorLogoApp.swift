350d3f2d82b880757139018b963f4c13acd74166
GArifullinII <ildaridrisovich@gmail.com>
2022-09-17 11:12:36 +0400

    Initial Commit

---
+++ MajorLogo/MajorLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajorLogoApp.swift
+//  MajorLogo
+//
+//  Created by Ildar Garifullin on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MajorLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
